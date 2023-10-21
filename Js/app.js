007f1098ca066adb2e53f761605bc3f25edf94b8
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2023-10-21 21:23:35 +0600

    responsive navbar completed

---
+++ Js/app.js
@@ -0,0 +1,8 @@
+// responsive nav
+
+function openNav(){
+    document.getElementById('responsive-nav').style.height = '100%'; 
+}
+function closeNav(){
+    document.getElementById('responsive-nav').style.height = '0%'; 
+}
